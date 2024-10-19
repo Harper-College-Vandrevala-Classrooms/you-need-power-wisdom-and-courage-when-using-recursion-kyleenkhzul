8337ea7fc11aaf3d1689fdc77a6e6683865fffb6
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-10-19 14:34:33 -0500

    finished lab

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,38 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+  
+  /*
+   * This function returns the nth triangular number
+   * @param n, int, nth triangular number
+   * @return, returns the value of the nth triangular number
+   */
+  public int value(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + value(n - 1);
 }
+
+  /*
+   * This function adds two nth triangular numbers
+   * @param n, first nth triangular number
+   * @param m, second nth triangular number
+   * @return, sum of triangular numbers
+   */
+  public int add(int n, int m) {
+    return (value(n) + value(m));
+  }
+
+    /*
+   * This function subtracts two nth triangular numbers
+   * @param n, first nth triangular number
+   * @param m, second nth triangular number
+   * @return, difference of triangular numbers
+   */
+  public int subtract(int n, int m) {
+    return (value(n) - value(m));
+  }
+}
+
+
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,9 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +14,71 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+   // Tests for the value method
+   @Test
+   void testValueOf0() {
+      assertEquals(0, calculator.value(0));
+   }
+
+   @Test
+   void testValueOf1() {
+      assertEquals(1, calculator.value(1));
+   }
+
+   @Test
+   void testValueOf5() {
+      assertEquals(15, calculator.value(5));
+   }
+
+   @Test
+   void testValueOf10() {
+      assertEquals(55, calculator.value(10));
+   }
+   
+   @Test
+   void testValueOf15() {
+      assertEquals(120, calculator.value(15));
+   }
+
+   @Test
+   void testValueOf30() {
+      assertEquals(465, calculator.value(30));
+   }
+
+   @Test
+   void testValueOf60() {
+      assertEquals(1830, calculator.value(60));
+   }
+
+   // Tests for add method
+   @Test
+   void testAdd1And1() {
+      assertEquals(2, calculator.add(1, 1));
+   }
+
+   @Test
+   void testAdd2And3() {
+      assertEquals(9, calculator.add(2, 3));
+   }
+
+   @Test
+   void testAdd4And2() {
+      assertEquals(13, calculator.add(4,2));
+   }
+
+   // Tests for subtract method
+   @Test
+   void testSubtract1And1() {
+      assertEquals(0, calculator.subtract(1, 1));
+   }
+
+   @Test
+   void testSubtract2And3() {
+      assertEquals(-3, calculator.subtract(2, 3));
+   }
+
+   @Test
+   void testSubtract4And2() {
+      assertEquals(7, calculator.subtract(4, 2)); 
+   }
 }
